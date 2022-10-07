80d5b76d30c7fc2d01b725b798e1afe4152c5376
pupil <pupil@ivkhk.ee>
2022-10-07 15:16:54 +0300

    add scanner

--- src/jktv21oopproba/App.java
+++ src/jktv21oopproba/App.java
@@ -1,6 +1,14 @@
 
+
+
+
+
+
+
 package jktv21oopproba;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import myclasses.Book;
 import tools.Tools;
 
@@ -9,9 +17,31 @@ public class App {
 //        Book book = new Book();
 //        book.setTitle("Learning JAVA");
 //        book.addAuthor(createAuthor("Antonio", "Gonsales"));
+        Scanner scanner = new Scanner(System.in);
         Tools tools = new Tools();
         Book book = tools.createBook("KOOOO");
         book.addAuthor(tools.createAuthor("Ivan", "Ivanov"));
         System.out.println(book.toString());
+        Book[] books = new Book[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Название книги"+(i+1)+": ");
+            String title = scanner.nextLine();
+            book = new Book();
+            book.setTitle(title);
+            System.out.println("Сколько авторов: ");
+            int countAuthorsInBook = scanner.nextInt();
+            scanner.nextLine();
+
+            for (int j = 0; j < countAuthorsInBook; j++) {
+                System.out.println("Имя автора"+(j+1)+": ");
+                String firstname = scanner.nextLine();
+                System.out.println("Фамилия автора"+(j+1)+": ");
+                String lastname = scanner.nextLine();
+                book.addAuthor(tools.createAuthor(firstname, lastname));
+            }
+            
+            books[i] = book;
+        }
+        System.out.println(Arrays.toString(books));
     }
 }
